bf405fd1a768cdb2a52c1319e92360a52cbcc541
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-29 18:54:53 -0700

    added tracks

--- Hashmatique.java
+++ Hashmatique.java
@@ -5,6 +5,9 @@ java.util.Hashmap;
 public class Hashmatique {
 
     HashMap<String, String> tracklist = new HashMap<String, String>();
-
+        tracklist.put("More Than A Feeling", "fsdakfljsdf afsdkfjs fsdk;ljfa s;kfljaf skdfjs d;lfjkas d;fjsa df");
+        tracklist.put("Dust In the Bottle", "fsdakfljsdf afsdkfjs fsdk;ljfa s;kfljaf skdfjs d;lfjkas d;fjsa df");
+        tracklist.put("Freebird", "fsdakfljsdf afsdkfjs fsdk;ljfa s;kfljaf skdfjs d;lfjkas d;fjsa df");
+        tracklist.put("Dark Stallion", "fsdakfljsdf afsdkfjs fsdk;ljfa s;kfljaf skdfjs d;lfjkas d;fjsa df");
 
 }
