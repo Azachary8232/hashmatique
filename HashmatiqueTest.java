bf869fbd8a00190f06901f9a57f48b78a2eec03a
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-29 19:47:39 -0700

    Got Track by Title

--- HashmatiqueTest.java
+++ HashmatiqueTest.java
@@ -1,11 +1,16 @@
-
+import java.util.HashMap;
 
 public class HashmatiqueTest{
 
     public static void main(String[] args){
 
-        Hashmatique HashFile = new Hashmatique();
 
-        
+        HashMap<String, String> tracklist = new HashMap<String, String>();
+        tracklist.put("More Than A Feeling", "fsdakfljsdf afsdkfjs fsdk;ljfa s;kfljaf skdfjs d;lfjkas d;fjsa df");
+        tracklist.put("Dust In the Bottle", "fsdakfljsdf afsdkfjs fsdk;ljfa s;kfljaf skdfjs d;lfjkas d;fjsa df");
+        tracklist.put("Freebird", "fsdakfljsdf afsdkfjs fsdk;ljfa s;kfljaf skdfjs d;lfjkas d;fjsa df");
+        tracklist.put("Dark Stallion", "fsdakfljsdf afsdkfjs fsdk;ljfa s;kfljaf skdfjs d;lfjkas d;fjsa df");
+
+        System.out.println(tracklist.get("More Than A Feeling"));
     }
 }
